b7b4e41ca5f41f64041b54c28b3567b4d1200937
Ashish Kumar <asdfrajkumar1122@gmail.com>
2023-07-13 18:34:24 +0530

    Update home_controller.js
    Home controller is updated

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,37 +1,29 @@
 const User = require('../models/user');
 const Review = require('../models/review');
 
-// Render the home page
 module.exports.home = async function (req, res) {
     try {
-        // Check if the user is authenticated
         if (!req.isAuthenticated()) {
             req.flash('error', 'Please sign in!');
             return res.redirect('/users/sign-in');
         }
 
-        // Find the logged-in user by ID
         let user = await User.findById(req.user.id);
-        
-        // Find all the reviews for the logged-in user
-        let review = await Review.find({ reviewTo: req.user.id }).sort('-createdAt');
-
         let recipient = [];
         let reviews = [];
 
-        // Find the users to review for the logged-in user
-        for (let i = 0; i < user.userToReview.length; i++) {
-            let userToReview = await User.findById(user.userToReview[i]);
-            recipient.push(userToReview);
-        }
+        if (user.isAdmin) {
+            // If the user is an admin, retrieve all reviews
+            reviews = await Review.find().sort('-createdAt');
+        } else {
+            // Find the reviews addressed to the logged-in user
+            reviews = await Review.find({ reviewTo: req.user.id }).sort('-createdAt');
 
-        // Prepare the reviews to be displayed
-        for (let i = 0; i < review.length; i++) {
-            let reviewToShow = {
-                name: review[i].reviewBy_name,
-                content: review[i].content
-            };
-            reviews.push(reviewToShow);
+            // Find the users to review for the logged-in user
+            for (let i = 0; i < user.userToReview.length; i++) {
+                let userToReview = await User.findById(user.userToReview[i]);
+                recipient.push(userToReview);
+            }
         }
 
         return res.render('home', {
